73f51828ed9cdd51d93c61bae82375d33e1b5888
Asuka <2232208323@qq.com>
2023-08-23 10:26:43 +0800

    2 end

--- model/lenet.py
+++ model/lenet.py
@@ -6,17 +6,21 @@ class LeNet(nn.Module):
     def __init__(self,classes) -> None:
         super(LeNet,self).__init__()
         self.conv1 = nn.Conv2d(3,6,5)
+        self.maxpool1 = nn.MaxPool2d(2,2)
         self.conv2 = nn.Conv2d(6,16,5)
+        self.maxpool2 = nn.MaxPool2d(2,2)
         self.fc1 = nn.Linear(16*5*5,120)
         self.fc2 = nn.Linear(120,84)
         self.fc3 = nn.Linear(84,classes)
 
     def forward(self,x):
         out = F.relu(self.conv1(x))
-        out = F.max_pool2d(out,2)
+        out = self.maxpool1(out)
         out = F.relu(self.conv2(out))
-        out = F.max_pool2d(out,2)
+        out = self.maxpool2(out)
         out = out.view(out.size(0),-1)
+        #out.size(0)就是batch_size,-1就是把三维展成1维，这里就是400
+        #out = out.view(out.size(0),400)
         out = F.relu(self.fc1(out))
         out = F.relu(self.fc2(out))
         out = self.fc3(out)
